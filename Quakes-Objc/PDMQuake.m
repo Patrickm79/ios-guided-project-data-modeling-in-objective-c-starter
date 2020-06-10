37e7bff2172706685ad1a70f2021b7d9b69677b0
Patrick Millet <lohs79seahawk@gmail.com>
2020-06-10 13:19:24 -0500

    Implemented initWithDictionary method and passing the Unit test that checks the parsing accuracy.

--- Quakes-Objc/PDMQuake.m
+++ Quakes-Objc/PDMQuake.m
@@ -36,13 +36,37 @@
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 {
     // Pull out data from dictionary
-    
+    NSDictionary *properties = dictionary[@"properties"]; // FIXME: Use constants for properties / attributes to prevent stringly typed errors
+    NSDictionary *geometry = dictionary[@"geometry"];
+    NSArray *coordinates = geometry[@"coordinates"];
     // If valid, create object and return it
-    
     // If invalid, return nil (failure)
+    // Create temporary values
     
+    NSNumber *magnitudeNumber = properties[@"mag"];
+    NSString *place = properties[@"place"];
+    NSNumber *timeNumber = properties[@"time"];
+    NSNumber *latitude = nil;
+    NSNumber *longitude = nil;
     
+    if (coordinates.count >= 2) { // Prevents index out of bounds crash
+        // Longitude is first in array based on API
+        latitude = coordinates[1];
+        longitude = coordinates[0];
+    } else {
+        NSLog(@"Missing coordinates"); // TODO: some UI response??
+    }
+    
+    // Required properties for valid object (not always a safe option and depends on data)
+    if (magnitudeNumber && place && timeNumber && latitude && longitude) {
+        
+        NSDate *time = [[NSDate alloc] initWithTimeIntervalSince1970:timeNumber.longValue / 1000.0];
+        
+        self = [self initWithMagnitude:magnitudeNumber.doubleValue place:place time:time latitude:latitude.doubleValue longitude:longitude.doubleValue];
+        return self;
+    } else { // Missing a property we consider essential, throw out the data.
     return nil;
+    }
 }
 
 @end
